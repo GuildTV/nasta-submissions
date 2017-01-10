71325d207a8bbc28786b08cbfc1a422e401ddc65
Julian Waller <git@julusian.co.uk>
2017-01-10 20:01:38 +0000

    Add a late warning to submisson received email

--- resources/views/emails/station/entry-submitted.blade.php
+++ resources/views/emails/station/entry-submitted.blade.php
@@ -7,6 +7,10 @@
 
   <p>This is to confirm your entry '{{ $entry->name }}' for the {{ $entry->category->name }} award.</p>
 
+  @if ($entry->isLate())
+  <p>Note: Your entry has been marked as late</p>
+  @endif
+
   <p>Remember that you can still edit this submission up until the deadline. You can do so <a href="{{ route('station.entry', $entry->category) }}">here</a>.</p>
 
   <p>Regards,<br/ >The Asset Acquisitions Team</p>
