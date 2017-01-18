1ea882fd0a9c64366f52fdfa9c728b7daa8cf901
Julian Waller <git@julusian.co.uk>
2017-01-18 00:58:49 +0000

    Seed with real stations

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -14,29 +14,24 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-      $user = User::create([
-        'name' => 'Test Admin',
-        'username' => 'test_admin',
-        'email' => 'test@email.com',
-        'type' => 'admin',
-        'password' => Hash::make('test123')
-      ]);
-      $user->save();
-      $user = User::create([
-        'name' => 'Test Judge',
-        'username' => 'test_judge',
-        'email' => 'judge@email.com',
-        'type' => 'judge',
-        'password' => Hash::make('test123')
-      ]);
-      $user->save();
-      $user = User::create([
-        'name' => 'Test Station',
-        'username' => 'test_station',
-        'email' => 'station@email.com',
-        'type' => 'station',
-        'password' => Hash::make('test123')
-      ]);
-      $user->save();
+      // User::create([
+      //   'name' => 'Test Admin',
+      //   'compact_name' => "TestAdmin",
+      //   'username' => 'test_admin',
+      //   'email' => 'test@email.com',
+      //   'type' => 'admin',
+      //   'password' => Hash::make('test123')
+      // ]);
+
+      // User::create([
+      //   'name' => 'Test Judge',
+      //   'compact_name' => "TestJudge",
+      //   'username' => 'test_judge',
+      //   'email' => 'judge@email.com',
+      //   'type' => 'judge',
+      //   'password' => Hash::make('test123')
+      // ]);
+
+      $this->call(StationsSeeder::class)
     }
 }
