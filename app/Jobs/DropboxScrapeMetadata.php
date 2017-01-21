9e822045626fc5a6e50d93449c039f5e11a8fe29
Julian Waller <git@julusian.co.uk>
2017-01-21 23:42:37 +0000

    Untested video metadata scraping

--- app/Jobs/DropboxScrapeMetadata.php
+++ app/Jobs/DropboxScrapeMetadata.php
@@ -11,6 +11,7 @@ use App\Mail\Admin\ExceptionEmail;
 use App\Helpers\Files\DropboxFileServiceHelper;
 
 use App\Database\Upload\UploadedFile;
+use App\Database\Upload\VideoMetadata;
 
 use Config;
 use Exception;
@@ -39,9 +40,64 @@ class DropboxScrapeMetadata implements ShouldQueue
      */
     public function handle()
     {
-        
+        $client = new DropboxFileServiceHelper($this->file->account->access_token);
+        $metadata = $client->getMetadata($this->file->path);
+        if ($metadata == null) {
+            Log::error("Failed to get metadata for file #" . $this->file->id);
+            return;
+        }
+
+        $res = $this->parseInfo($metadata);
+        if ($res === false) {
+            if ($this->attempts() < 5) {
+                return $this->release(120); // try again in 120s
+            }
+            return null;
+        }
+
+        if ($this->file->video_metadata_id != null){
+            VideoMetadata::where('id', $this->file->video_metadata_id)->update($res);
+        } else {
+            $data = VideoMetadata::create($res);
+            $this->file->video_metadata_id = $data->id;
+            $this->file->save();
+        }
+
+        UploadedFileLog::create([
+            'station_id' => $this->file->station->id,
+            'category_id' => $this->file->category_id,
+            'level' => 'info',
+            'message' => 'Scraped metadata for file \'' . $file['name'] . '\' (#' . $this->file->id . ')',
+        ]);
 
         return true;
+    }
+
+    private function parseInfo($metadata){
+        if (!isset($metadata['media_info']))
+            return false;
+
+        if (isset($metadata['media_info']['pending']))
+            return null;
+
+        if (!isset($metadata['media_info']['metadata']))
+            return null;
+
+        $data = $metadata['media_info']['metadata'];
+        if (!isset($data['video']))
+            return null;
+
+        if (!isset($data['video']['dimensions']))
+            return null;
+
+        if (!isset($data['video']['dimensions']['width']) || !isset($data['video']['dimensions']['height']) || !isset($data['video']['duration']))
+            return null;
+
+        return [
+            'height' => intval($data['video']['dimensions']['height']),
+            'width' => intval($data['video']['dimensions']['width']),
+            'duration' => intval($data['video']['duration'])
+        ];
     }
 
     /**
@@ -52,6 +108,13 @@ class DropboxScrapeMetadata implements ShouldQueue
      */
     public function failed(Exception $exception)
     {
+        UploadedFileLog::create([
+            'station_id' => $this->file->station->id,
+            'category_id' => $this->file->category_id,
+            'level' => 'error',
+            'message' => 'Failed to get metadata for file \'' . $file['name'] . '\' (#' . $this->file->id . ')',
+        ]);
+
         ExceptionEmail::notifyAdmin($exception, "Dropdox scrape metadata: File #" . $this->file->id);
     }
 }
---
+++ database/migrations/2017_01_21_201651_create_video_metadata_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVideoMetadataTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('video_metadata', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->integer('width');
+            $table->integer('height');
+            $table->integer('duration');
+
+            $table->timestamps();
+        });
+
+        Schema::table('uploaded_files', function (Blueprint $table) {
+            $table->integer('video_metadata_id')->unsigned()->after('public_url')->unique()->nullable()->default(null);
+
+            $table->foreign('video_metadata_id')->references('id')->on('video_metadata')->onDelete('cascade');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('uploaded_files', function (Blueprint $table) {
+            $table->dropColumn('video_metadata_id');
+        });
+
+        Schema::dropIfExists('video_metadata');
+    }
+}
